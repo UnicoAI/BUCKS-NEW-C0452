2d9bc2beb4382351e74c079062c4d903b81ac345
Marius Daniel Boncica <102974083+UnicoAI@users.noreply.github.com>
2022-11-10 20:08:04 +0000

    Update Main.java

--- CW1_Marius_Boncica/Main.java
+++ CW1_Marius_Boncica/Main.java
@@ -1,8 +1,23 @@
 
 
 import java.util.*;
+/**
+ 
+ * @author     Marius Daniel Boncica Student Bucks New ID: 22045429
+ * @version    0.1 (2022.10.29)
+ * 
+ * Modified by Marius Boncica 01/11/2022
 
+ */
 public class Main {
+/**
+ 
+ * @author     Marius Daniel Boncica Student Bucks New ID: 22045429
+ * @version    0.1 (2022.10.29)
+ * 
+ * Modified by Marius Boncica 01/11/2022
+
+ */
 
     private static ArrayList<Album> albums = new ArrayList<>();
 
@@ -15,14 +30,14 @@ public class Main {
         album.addSong("Flowers","ArrDee",662342);
         album.addSong("Seventeen Going Under","Sam Fender",65763432);
         album.addSong("Overseas","D-Block Europe",65763432);
-        album.addSong("Come on home for Christmas","George Ezra",65763432);
+        album.addSong("Come on home for Christmas","George Ezra",65763442);
         album.addSong("Sacrifice","The Weeknd",65763432);
         album.addSong("War","ArrDee & Aitch ",65763632);
         album.addSong("BMW","Bad Boy Chiller Crew",69763432);
         album.addSong("Starlight","Davw",654233432);
         album.addSong("Go","Cat Burns",65763432);
         album.addSong("Falling Back","Drake",65763432);
-        album.addSong("Forget me","Lewis Capaldi",65763432);
+        album.addSong("Forget me","Lewis Capaldi",68888882);
        
         albums.add(album);  
 //create a LinkedList of songs to behave like a playlist to who we add songs from the arraylist
@@ -46,7 +61,7 @@ public class Main {
     }
 
 
-    private static boolean play(LinkedList<Song> playList){
+    private static void play(LinkedList<Song> playList){
         printMenu();
         Scanner sc = new Scanner(System.in); //open scanner for user input
         boolean quit = false;
@@ -56,8 +71,8 @@ public class Main {
         if(playList.size() == 0){
             System.out.println("This playlist have no song");
         }else {
-            System.out.println("Now playing " + listIterator.next().toString());
-            
+            System.out.println("Now playing " +"\n"+ listIterator.next().toString());
+      
         }
 
         while(!quit){
@@ -68,6 +83,7 @@ public class Main {
 
                 case 0:
                     System.out.println("Goodbye!");
+                    System.out.println("===================================================================================");
                     quit = true;
                     break;
 
@@ -82,6 +98,7 @@ public class Main {
                         System.out.println("Now playing "+listIterator.next().toString());
                     }else {
                         System.out.println("no song availble, reached to the end of the list");
+                        System.out.println("===================================================================================");
                         forward = false;
                     }
                     break;
@@ -96,6 +113,7 @@ public class Main {
                         System.out.println("Now playing "+listIterator.previous().toString());
                     }else {
                         System.out.println("we are the first song");
+                        System.out.println("===================================================================================");
                         forward = false;
                     }
                     break;
@@ -103,23 +121,33 @@ public class Main {
                 case 3: //message alert when we reach the start of the linkedlist
                     if(forward){
                         if(listIterator.hasPrevious()){
-                            System.out.println("Now playing "+listIterator.previous().toString());
+                            System.out.println("Now playing "+"\n" +listIterator.previous().toString());
+                            System.out.println("===================================================================================");
                             forward = false;
                         }else {
                             System.out.println("we are at the start of the list");
+                            System.out.println("===================================================================================");
                         }
                     }else { ////message alert when we reach the end of the linkedlist
                         if(listIterator.hasNext()){
                             System.out.println("now playing "+listIterator.next().toString());
+                            System.out.println("===================================================================================");
                             forward = true;
                         }else {
                             System.out.println("we have reached to the end of list");
+                            System.out.println("===================================================================================");
                             
                         }
                     }
                     break;
 
                 case 4: //call method to print playlist songs
+                System.out.println("===================================================================================");
+                            
+                System.out.println(
+                    "||         Title          " + " ||     Artist Name          ||" + "     Play Count                ||");
+                    System.out.println("===================================================================================");
+                            
                     printList(playList);
                     
                    
@@ -130,80 +158,128 @@ public class Main {
                     case 6: // searching for playcount number
                     Scanner newcount = new Scanner(System.in);
                     System.out.println("What number of PlayCount are you looking for?");
-                     Double ssss = newcount.nextDouble();
+                     int ssss = newcount.nextInt();
                     
 
-                     for(Song checkedSong : playList){
-            if (checkedSong.getPlaycount().equals(ssss)){
+                     for(Song Song: playList){
+            if (Song.getPlaycount() == ssss){
              
-                       System.out.println(ssss + "  is the Play Count number for  " + checkedSong.getTitle() + " --Arrtist Name--" + checkedSong.getArtist());
+                       System.out.println(ssss + "  is the Play Count number for  " + Song.getTitle() + " --Arrtist Name--" + Song.getArtist());
+                       System.out.println("===================================================================================");
+                       for (int index = 0; index < playList.size(); index++)
+                       {        
+                         Song s = playList.get(index);
+                         if (s.getPlaycount() < Song.getPlaycount())
+                         {
+                             System.out.print(playList.get(index+1));
+                            printMenu();
+                            
+                         } else
+                         {}
+                 
                     }
+            }
+        }
                 
-                  return false;
-                    
-                }
-                    break;
+
+                 
+          
+            
+               
                      
                 
                 case 7:  //remove songs from playlist using iterator
                     if (playList.size() >0){
                         listIterator.remove();
                         if(listIterator.hasNext()){
-                            System.out.println("now playing "+listIterator.next().toString());
-                        }
+                            System.out.println("Now Playing "+ "\n"+listIterator.next().toString());
+                       }
                         else {
                             if(listIterator.hasPrevious())
-                            System.out.println("now playing "+listIterator.previous().toString());
-                        }
+                            System.out.println("Now Playing "+ "\n"+listIterator.previous().toString());
+                       }
                     }
                     break;
                     case 8: //add songs to the playlist using only title of the song
                     Scanner newsong = new Scanner(System.in);
+                    System.out.println("===================================================================================");
                     System.out.println("Wich song from our collection do you wanna add to a new playlist?");
                     System.out.println("Available Songs are: ");
                     System.out.println("Falling Back");
                     System.out.println("Forget me ");
+                    System.out.println("===================================================================================");
 
                     
                     
                     albums.get(0).addToPlayList(newsong.nextLine(), playList);
                     
-              
-       System.out.println("Song added to the new playlist:  " + newsong.nextLine());
+                    System.out.println(playList); // print playlist to see result off adding new song    
+       System.out.println("Song " + newsong.nextLine()+ "added to the new playlist  ");
+       System.out.println("===================================================================================");
+     
        printMenu();
-                    
-                    }
+       case 9: //remove song by index number
+       Scanner newcount1 = new Scanner(System.in);
+                    System.out.println("What is the song number you wish to delete");
+                     int sssss = newcount1.nextInt();
+                    playList.remove(sssss);
+                    System.out.println("Song list number  " +sssss  + "  removed from playlist");
+                printMenu();
+        case 10: //upload new song to database
+        System.out.println("What is the song Title you wish to add?");
+        Scanner newaddt = new Scanner(System.in);
+        System.out.println("What is the song Artist Name you wish to add?");
+        Scanner newadda = new Scanner(System.in);
+        System.out.println("What is the song Plays you wish to add?");
+        Scanner newaddp = new Scanner(System.in);
+        Song newa = new Song(newaddt.nextLine(), newadda.nextLine(), newaddp.nextInt());
+        playList.add(newa);
+        printList(playList);
+        printMenu();
+        System.out.println(newa.toString() +"\n" + "Added to the playlist");
 
-            }
-        return forward;
-        
+                }
+                }
+          
         }
     
 
    
 
     private static void printMenu(){  //menu list
-        System.out.println("Available options\n press");
-        System.out.println("0 - to quit\n"+
-                "1 - to play next song\n"+
-                "2 - to play previous song\n"+
-                "3 - to replay the current song\n"+
-                "4 - list of all songs \n"+
-                "5 - print all available options\n"+
-                "6 - find by playcount song \n" +
-                "7- delete current song \n" +
-                "8 - add new song");
+        System.out.println(
+              "===================================================================================");
+        System.out.println(
+                "     ||||||||||||||||||||            MENU            |||||||||||||||||||||\n ");
+        System.out.println(
+                "===================================================================================");
+        System.out.println(
+                "||     0 - To Quit                                                               ||\n"+
+                "||     1 - To Play Next Song                                                     ||\n"+
+                "||     2 - To Play Previous Song                                                 ||\n"+
+                "||     3 - To Replay The Current Song                                            ||\n"+
+                "||     4 - Print Playlist                                                        ||\n"+
+                "||     5 - MENU                                                                  ||\n"+
+                "||     6 - Find And Play A Playlist By the Play Count Number                     ||\n" +
+                "||     7 - Delete Current Song                                                   ||\n" +
+                "||     8 - Add New Song                                                          ||\n" +
+                "||     9 - Remove Song By index Number                                           ||\n" +
+                "||     10 - Upload New Song                                                      ||\n"
+              
+                );
+                System.out.println(
+             "===================================================================================");
     }
 
     private static void printList(LinkedList<Song> playList){ //style the console
         Iterator<Song> iterator = playList.iterator();
-        System.out.println("-----------------");
+        System.out.println("===================================================================================");
 
         while (iterator.hasNext()){
             System.out.println(iterator.next());
         }
 
-        System.out.println("-----------------");
+        System.out.println("===================================================================================");
     }
     
 
